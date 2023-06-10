5d368cfab3991f35c9eae42769429adf08d06c26
Bryan Stitt <bryan@stitthappens.com>
2023-06-09 18:27:13 -0700

    subscribe to multiple chains at once

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 Subscribe to a Web3 RPC and do something when an event is seen
 
-Initially, this will be hard coded for web3-proxy deposit transactions,
-but it would probably be useful generally.
+For now, this is hard coded for LlamaNodes Payment Factory deposit events,
+but it could be useful generally.
 
 ## Running
 
 ```bash
-W3TTT_RPC_WS_URL=wss://polygon.llamarpc.com cargo run
+export W3TTT_PROXY_URLS=eth.llamarpc.com,polygon.llamarpc.com
+cargo run
 ```
 
 ## Development
@@ -22,8 +23,7 @@ The README.md is kept up-to-date with `cargo readme > README.md`
 
 ## Todo
 
-- [ ] proper logging
-- [ ] moka cache for blocks
+- [x] proper logging
 - [ ] petgraph for tracking forks?
-- [ ] retry rather than exit
+- [x] retry rather than exit
 - [ ] handle orphaned transactions
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 //! Subscribe to a Web3 RPC and do something when an event is seen
 //!
-//! Initially, this will be hard coded for web3-proxy deposit transactions,
-//! but it would probably be useful generally.
+//! For now, this is hard coded for LlamaNodes Payment Factory deposit events,
+//! but it could be useful generally.
 //!
 //! # Running
 //!
 //! ```bash
-//! W3TTT_RPC_WS_URL=wss://polygon.llamarpc.com cargo run
+//! export W3TTT_PROXY_URLS=eth.llamarpc.com,polygon.llamarpc.com
+//! cargo run
 //! ```
 //!
 //! # Development
@@ -20,41 +21,27 @@
 //!
 //! # Todo
 //!
-//! - [ ] proper logging
-//! - [ ] moka cache for blocks
+//! - [x] proper logging
 //! - [ ] petgraph for tracking forks?
-//! - [ ] retry rather than exit
+//! - [x] retry rather than exit
+//! - [ ] handle orphaned transactions
 //!
-//!
-//!
-
-// TODO: decide how to subscribe to logs
-/*
-I've heard multiple reports that subcribing to logs with a websocket occassionaly miss logs. I see a few options.
-
-1. just subscribe to logs and if one gets missed, the user can report their txid/address and we can manually submit the tx
-2. simple process
-    a. subscribe to new heads
-    b. when a block arrives, call eth_getLogs with our topics for every block we haven't seen
-3. complex process (only make backend requests if there is a relevant log)
-    a. subscribe to new heads
-    b. when a block arrives, get the log_bloom for every block we haven't seen
-    c. check the bloom filter for any logs we care about
-    d. if the bloom filter returns false, skip this block
-    e. call eth_getLogs with our topics
-
-if a re-org happens, we should POST the txid
-*/
-
 use anyhow::Context;
 use dotenv::dotenv;
 use ethers::{
     abi::ethereum_types::BloomInput,
     prelude::{abigen, LogMeta},
     providers::{Middleware, Provider, StreamExt, Ws},
-    types::{Address, ValueOrArray},
+    types::{Address, Block, TxHash, ValueOrArray},
 };
-use std::{env, sync::Arc};
+use futures::future::try_join_all;
+use reqwest::Client;
+use serde::Deserialize;
+use std::{env, sync::Arc, time::Duration};
+use tokio::time::sleep;
+use tracing::{debug, error, info, info_span, trace, Instrument};
+
+type EthersProviderWs = Provider<Ws>;
 
 // TODO: do this at runtime so we can listen to multiple events
 abigen!(
@@ -66,39 +53,159 @@ abigen!(
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     // use dotenv to get config
     let _ = dotenv().ok();
 
-    let rpc_ws_url = env::var("W3TTT_RPC_WS_URL")
-        .context("Setting W3TTT_RPC_WS_URL in your environment is required")?;
+    let proxy_urls = env::var("W3TTT_PROXY_URLS")
+        .context("Setting W3TTT_PROXY_URLS in your environment is required")?;
 
-    let factory_address = env::var("W3TTT_LLAMANODES_FACTORY")
-        .unwrap_or("0x4e3BC2054788De923A04936C6ADdB99A05B0Ea36".to_string())
-        .parse::<Address>()
-        .context("W3TTT_LLAMANODES_FACTORY is not a valid address")?;
+    let http_client = reqwest::Client::new();
 
-    // TODO: prometheus metrics?
-    // TODO: dotfile exporter for the chain?
+    let handles = proxy_urls.split(',').map(|proxy_url| {
+        let f = run_forever(http_client.clone(), proxy_url.to_owned());
 
-    // TODO: acquire mysql lock? multiple reports for the same event doesn't really hurt anything
+        // TODO: prometheus metrics?
+        // TODO: dotfile exporter for the chain?
 
-    // TODO: create a websocket connection to an rpc provider
+        tokio::spawn(f)
+    });
+
+    try_join_all(handles).await?;
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct StatusPage {
+    payment_factory_address: Option<Address>,
+}
+
+async fn run_forever(http_client: Client, proxy_url: String) {
+    loop {
+        if let Err(err) = run(&http_client, &proxy_url).await {
+            error!("{} errored! {:?}", proxy_url, err);
+        }
+        sleep(Duration::from_secs(10)).await;
+    }
+}
+
+async fn run(http_client: &Client, proxy_url: &str) -> anyhow::Result<()> {
+    let (http_scheme, ws_scheme) = if proxy_url.contains("localhost") {
+        ("http", "ws")
+    } else {
+        ("https", "wss")
+    };
+
+    let http_url = format!("{}://{}", http_scheme, proxy_url);
+    let ws_url = format!("{}://{}", ws_scheme, proxy_url);
+
+    let status_url = format!("{}/status", http_url);
+
+    let status_json: StatusPage = http_client
+        .get(status_url)
+        .send()
+        .await?
+        .json()
+        .await
+        .context("unexpected format of /status")?;
+
+    let factory_address = status_json.payment_factory_address.unwrap_or_else(|| {
+        "0x4e3BC2054788De923A04936C6ADdB99A05B0Ea36"
+            .parse()
+            .unwrap()
+    });
+
+    // TODO: acquire mysql lock? multiple reports for the same transaction doesn't really hurt anything
+
+    // create a websocket connection to an rpc provider
     let provider = Arc::new(
-        Provider::<Ws>::connect(rpc_ws_url)
+        Provider::<Ws>::connect_with_reconnects(ws_url, usize::MAX)
             .await
             .context("failed connecting to websocket")?,
     );
 
     // TODO: query mysql (or maybe redis) to know the last row we processed
     // 40157242 (march 9, 2023) is when 0x4e3bc2054788de923a04936c6addb99a05b0ea36 was created
-    // TODO: last_processed should be a number and a hash (or maybe the full block object)
     let mut last_processed = provider
-        .get_block(40157242)
+        .get_block(41380731)
         .await
         .context("failed fetching last processed block")?
-        .expect("there should always be a block");
+        .context("last_processed block should always exist")?;
 
     let factory = LlamaNodes_PaymentContracts_Factory::new(factory_address, provider.clone());
+
+    let mut new_heads_sub = provider.subscribe_blocks().await?;
+
+    while let Some(new_head) = new_heads_sub.next().await {
+        // TODO: will need to handle reorgs. will need to find the common ancestor
+        // TODO: lag by X blocks
+        let new_head_number = new_head.number.unwrap().as_u64();
+        let new_head_hash = new_head.hash.unwrap();
+
+        let last_number = last_processed.number.unwrap().as_u64();
+        let last_hash = last_processed.hash.unwrap();
+
+        if new_head_hash == last_hash {
+            // we've already seen this block
+            continue;
+        }
+
+        // TODO: don't exit on error. sleep and retry
+        for i in last_number..new_head_number {
+            let block = provider
+                .get_block(i)
+                .await
+                .context("failed fetching old block")?
+                .context("there should always be a block")?;
+
+            let span = info_span!(
+                "process_block",
+                num=%block.number.unwrap(),
+                hash=?block.hash.unwrap(),
+                http_url,
+            );
+
+            process_block(&block, http_client, &factory, &http_url)
+                .instrument(span)
+                .await?;
+
+            // TODO: save last_processed somewhere external in case this process exits
+            last_processed = block;
+        }
+
+        let span = info_span!(
+            "process_block",
+            num=%new_head.number.unwrap(),
+            hash=?new_head.hash.unwrap(),
+            http_url,
+        );
+
+        process_block(&new_head, http_client, &factory, &http_url)
+            .instrument(span)
+            .await?;
+
+        last_processed = new_head;
+
+        // TODO: save last_processed somewhere external in case this process exits
+    }
+
+    Ok(())
+}
+
+pub async fn process_block(
+    block: &Block<TxHash>,
+    http_client: &Client,
+    factory: &LlamaNodes_PaymentContracts_Factory<EthersProviderWs>,
+    proxy_http_url: &str,
+) -> anyhow::Result<()> {
+    debug!("checking logs_bloom");
+
+    // TODO: can we check multiple inputs at the same time?
+    let logs_bloom = block
+        .logs_bloom
+        .context("blocks here should always have a bloom")?;
 
     let payment_received_filter = factory.payment_received_filter();
 
@@ -110,66 +217,55 @@ async fn main() -> anyhow::Result<()> {
         panic!("topic0 should always be set");
     };
 
-    let mut new_heads_sub = provider.subscribe_blocks().await?;
+    // check the topic bloom first because it has more bits and so hopefully has less common false positives
+    let topics_bloom_input = BloomInput::Hash(payment_received_topic0.as_fixed_bytes());
 
-    while let Some(new_head) = new_heads_sub.next().await {
-        // TODO: move this to a seperate function that is spawned
-        // TODO: don't just query new_head. work backwards from new_head to last_processed
-        // TODO: working backwards will need thought to handle reorgs. will need to find the common ancestor
-        let new_head_number = new_head.number.unwrap();
-        let new_head_hash = new_head.hash.unwrap();
-        let new_head_parent = new_head.parent_hash;
+    if !logs_bloom.contains_input(topics_bloom_input) {
+        trace!("block does not contain logs using the payment_received event");
+        return Ok(());
+    }
 
-        if new_head_hash == last_processed.hash.unwrap() {
-            // we've already seen this block
-            continue;
-        }
+    // next check the contract address
+    let factory_address = factory.address();
+    let address_bloom_input = BloomInput::Raw(factory_address.as_bytes());
 
-        println!("new_head: {} {:#?}", new_head_number, new_head_hash,);
+    if !logs_bloom.contains_input(address_bloom_input) {
+        trace!("block does not contain logs using the factory address");
+        return Ok(());
+    }
 
-        if new_head_parent != last_processed.hash.unwrap() {
-            // TODO: work backwards from new_head until last_processed is the parent or ancestor of the processed block
-        }
+    // bloom filters will never have a false negative
+    // there can be false positives, but bloom filter checks cut out a lot of unnecessary eth_getLogs
+    trace!("bloom filters passed");
 
-        // TODO: can we check multiple inputs at the same time?
-        let logs_bloom = new_head.logs_bloom.unwrap();
+    let block_hash = block.hash.unwrap();
 
-        // check the topic bloom first because it has more bits and so hopefully has less common false positives
-        let topics_bloom_input = BloomInput::Hash(payment_received_topic0.as_fixed_bytes());
+    // filter for the logs only on this new head block
+    let payment_received_filter = factory.payment_received_filter().at_block_hash(block_hash);
 
-        if !logs_bloom.contains_input(topics_bloom_input) {
-            last_processed = new_head;
-            continue;
-        }
+    // rust-analyzer loses this type
+    let logs_with_meta: Vec<(PaymentReceivedFilter, LogMeta)> =
+        payment_received_filter.query_with_meta().await?;
 
-        // next check the contract address
-        let address_bloom_input = BloomInput::Raw(factory_address.as_bytes());
+    trace!(?logs_with_meta, "from payment received filter");
 
-        if !logs_bloom.contains_input(address_bloom_input) {
-            last_processed = new_head;
-            continue;
-        }
+    for (_, log_meta) in logs_with_meta {
+        let txid = log_meta.transaction_hash;
 
-        // there can be false positives, but bloom filter checks cut out a lot of unnecessary eth_getLogs
+        info!(?txid, "submitting transaction");
 
-        // filter for the logs only on this new head block
-        let payment_received_filter = factory
-            .payment_received_filter()
-            .at_block_hash(new_head_hash);
+        // TODO: submit the txid to a new endpoint on web3-proxy
+        // TODO: post in the body instead?
+        let r = http_client
+            .post(format!("{}/user/balance/{:?}", proxy_http_url, txid))
+            .send()
+            .await?;
 
-        // rust-analyzer loses this type
-        let logs_with_meta: Vec<(PaymentReceivedFilter, LogMeta)> =
-            payment_received_filter.query_with_meta().await?;
+        info!(?r, "transaction submitted");
 
-        // println!("logs with meta: {:#?}", logs_with_meta);
+        let j = r.text().await?;
 
-        for (_, log_meta) in logs_with_meta {
-            let txid = log_meta.transaction_hash;
-
-            // TODO: submit the txid to a new endpoint on web3-proxy
-        }
-
-        last_processed = new_head;
+        info!(?j, "transaction submitted");
     }
 
     Ok(())
